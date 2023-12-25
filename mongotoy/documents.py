0a865b2b278ca145b67d95ca672476fcadb9c4b2
gurcuff91 <gf.meneses91@gmail.com>
2023-12-25 18:22:20 -0500

    Add more comments

--- mongotoy/documents.py
+++ mongotoy/documents.py
@@ -14,7 +14,21 @@ _REGISTERED_DOCS = OrderedDict()
 
 
 class BaseDocMeta(abc.ABCMeta):
-    
+    """
+    Metaclass for defining the behavior of classes derived from `BaseDoc`.
+    This metaclass is responsible for combining fields from base classes and the current class,
+    registering the document class, and ensuring unique document names.
+
+    Args:
+        name (str): Name of the class.
+        bases (tuple): Tuple of base classes.
+        namespace (dict): Dictionary containing the class attributes.
+        **kwargs: Additional keyword arguments.
+
+    Raises:
+        DocumentError: If a document with the same name is already registered.
+    """
+        
     def __new__(mcls, name, bases, namespace, **kwargs):
         _cls = super().__new__(mcls, name, bases, namespace)
         
@@ -35,13 +49,25 @@ class BaseDocMeta(abc.ABCMeta):
         
         # register document cls
         if name in _REGISTERED_DOCS:
-            raise DocumentError(f'Document {name} already defined, please user diferent name')
+            raise DocumentError(f'Document {name} already defined, please use a different name')
         _REGISTERED_DOCS[name] = _cls
         
         return _cls
     
     
 class DocMeta(BaseDocMeta):
+    """
+    Metaclass for defining the behavior of classes derived from `Document` with additional document-specific features.
+
+    Args:
+        name (str): Name of the class.
+        bases (tuple): Tuple of base classes.
+        namespace (dict): Dictionary containing the class attributes.
+        **kwargs: Additional keyword arguments.
+
+    Raises:
+        DocumentError: If no id field is declared or if multiple id fields are declared.
+    """
     
     def __new__(mcls, name, bases, namespace, **kwargs):
         _cls = super().__new__(mcls, name, bases, namespace)
@@ -53,16 +79,36 @@ class DocMeta(BaseDocMeta):
         if len(_id_fields) == 0:
             raise DocumentError(f'No id field declared into document {_cls}')
         if len(_id_fields) > 1:
-            raise DocumentError(f'To many id fields declared into document {_cls}.{_id_fields}') 
+            raise DocumentError(f'Too many id fields declared into document {_cls}.{_id_fields}') 
         
         return _cls
         
         
 class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
+    """
+    Base class for defining documents with fields in a structured manner.
+
+    Attributes:
+        __fields__ (dict): A dictionary to store field information.
+        __data__ (dict): A dictionary to store document data.
+
+    Methods:
+        __init_instance__: Initializes an instance of the document with the given data.
+        empty: Creates an empty instance of the document.
+        parse: Parses a dictionary into a document instance.
+        get_indexes: Retrieves a list of MongoDB index models based on document fields.
+        dump_bson: Dumps the document data into a BSON format.
+
+    Args:
+        **data: Keyword arguments for initializing document data.
+
+    Raises:
+        DocumentValidationError: If there are validation errors during document initialization.
+    """
     if TYPE_CHECKING:
         __fields__: dict[str, FieldInfo]
         __data__: dict[str, Any]
-        
+
     def __init__(self, **data): 
         self.__init_instance__(self, data)
         
@@ -76,6 +122,19 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
         parse_bson: bool = False,
         **options
     ):
+        """
+        Initializes an instance of the document with the given data.
+
+        Args:        
+            instance: The document instance to initialize.
+            data: Data to parse into the document.
+            use_defaults: If True, uses default values for missing fields.
+            strict: If True, raises errors for unknown data types.
+            parse_bson: If True, parses BSON data.
+
+        Raises:
+            DocumentValidationError: If there are validation errors during document initialization.
+        """
         instance.__data__ = {}
         errors = []
         for field in cls.__fields__.values():
@@ -97,9 +156,18 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
                 errors=errors,
                 document_cls=cls,
             )
-            
+
     @classmethod
     def empty(cls, use_defaults: bool = False) -> 'BaseDoc':
+        """
+        Creates an empty instance of the document.
+
+        Args:
+            use_defaults: If True, uses default values for fields.
+
+        Returns:
+            BaseDoc: An empty instance of the document.
+        """
         instance = cls.__new__(cls)
         cls.__init_instance__(instance, data={}, use_defaults=use_defaults)
         return instance
@@ -113,6 +181,18 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
         parse_bson: bool = False,
         **options
     ) -> 'BaseDoc':
+        """
+        Parses data into a document instance.
+
+        Args:
+            data: Data to parse into the document.
+            use_defaults: If True, uses default values for missing fields.
+            strict: If True, raises errors for unknown data types.
+            parse_bson: If True, parses BSON data.
+
+        Returns:
+            BaseDoc: A document instance.
+        """
         instance = cls.__new__(cls)
         cls.__init_instance__(
             instance,
@@ -126,6 +206,12 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
     
     @classmethod
     def get_indexes(cls) -> list[pymongo.IndexModel]:
+        """
+        Retrieves a list of MongoDB index models based on document fields.
+
+        Returns:
+            list: A list of pymongo.IndexModel instances.
+        """
         indexes = []
 
         # add field indexes
@@ -166,24 +252,64 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
         return indexes
     
     def __getitem__(self, key):
+        """
+        Gets the value of a field by key.
+
+        Args:
+            key: The key (field name or alias) to retrieve.
+
+        Returns:
+            Any: The value of the field.
+
+        Raises:
+            DocumentError: If the field is not found or not declared.
+        """
         field = self.__fields__.get(key)
         if not field:
             raise DocumentError(f'Field {key} not found or not declared yet')
         return field.__get__(self, self.__class__)
     
     def __setitem__(self, key, item):
+        """
+        Sets the value of a field by key.
+
+        Args:
+            key: The key (field name or alias) to set.
+            item: The value to set.
+
+        Raises:
+            DocumentError: If the field is not found or not declared.
+        """
         field = self.__fields__.get(key)
         if not field:
             raise DocumentError(f'Field {key} not found or not declared yet')
         field.__set__(self, item)
     
     def __delitem__(self, key):
+        """
+        Deletes the value of a field by key.
+
+        Args:
+            key: The key (field name or alias) to delete.
+
+        Raises:
+            DocumentError: If the field is not found or not declared.
+        """
         field = self.__fields__.get(key)
         if not field:
             raise DocumentError(f'Field {key} not found or not declared yet')
         field.__delete__(self)
     
     def dump_bson(self, by_alias: bool = True) -> bson.SON:
+        """
+        Dumps the document data into a BSON format.
+
+        Args:
+            by_alias: If True, uses field aliases as keys.
+
+        Returns:
+            bson.SON: A BSON document.
+        """
         data = {}
         for field in self.__fields__.values():
             key = field.alias if by_alias else field.name
@@ -195,12 +321,46 @@ class BaseDoc(abc.ABC, metaclass=BaseDocMeta):
     
     
 class EmbeddedDocument(BaseDoc):
+    """
+    Base class for defining embedded documents within other documents.
+
+    Example:
+        ```python
+        class Address(EmbeddedDocument):
+            street = StringField()
+            city = StringField()
+            zip_code = StringField()
+        ```
+
+    Inherited Attributes:
+        __fields__ (dict): A dictionary to store field information.
+        __data__ (dict): A dictionary to store document data.
+    """
     pass
 
-
 class Document(BaseDoc, metaclass=DocMeta):
+    """
+    Base class for defining top-level documents with MongoDB-specific configuration.
+
+    Example:
+        ```python
+        class MyDocument(Document):
+            field1 = StringField()
+            field2 = IntField()
+        ```
+
+    Inherited Attributes:
+        __fields__ (dict): A dictionary to store field information.
+        __data__ (dict): A dictionary to store document data.
+
+    Metaclass Attributes:
+        document_config (dict): MongoDB-specific configuration for the document.
+
+    Attributes:
+        id (ObjectId): An ObjectId field for the document ID with default settings.
+    """
     if TYPE_CHECKING:
         document_config: dict
-        
+
     id = fields.ObjectIdField(id_field=True, default_factory=lambda: bson.ObjectId())
         
--- mongotoy/errors.py
+++ mongotoy/errors.py
@@ -1,29 +1,86 @@
 from typing import TYPE_CHECKING, Type
 
 if TYPE_CHECKING:
-    from mongotoy.documents import BaseDocument
+    from mongotoy.documents import BaseDoc
 
 
 class ErrorWrapper(Exception):
+    """
+    Wrapper class for handling errors in the mongotoy library.
+
+    Args:
+        - loc (str): The location where the error occurred.
+        - error (Exception): The wrapped error instance.
+
+    Properties:
+        - loc (tuple[str]): The location where the error occurred, represented as a tuple of strings.
+        - error (Exception): The wrapped error instance.
+
+    Methods:
+        - dump_dict(): Convert the error information to a dictionary for easy serialization.
+
+    Example:
+        ```python
+        try:
+            # Some code that may raise an exception
+            ...
+        except Exception as e:
+            # Wrap the error with location information
+            wrapped_error = ErrorWrapper(loc='module.function', error=e)
+            # Access the wrapped error details
+            location = wrapped_error.loc
+            original_error = wrapped_error.error
+            # Dump the error information as a dictionary
+            error_dict = wrapped_error.dump_dict()
+        ```
+    """
 
     def __init__(self, loc: str, error: Exception):
         self._loc, self._error = self._unwrap_error((loc,), error)
         super().__init__(str(self._error))
 
     def _unwrap_error(self, loc: tuple[str], error: Exception) -> tuple[tuple[str], Exception]:
+        """
+        Recursively unwrap nested ErrorWrapper instances to get the original error and its location.
+
+        Args:
+            - loc (tuple[str]): The current location information.
+            - error (Exception): The wrapped error instance.
+
+        Returns:
+            tuple[tuple[str], Exception]: The final location information and the original error.
+        """
         if not isinstance(error, ErrorWrapper):
             return loc, error
         return self._unwrap_error((*loc, *error.loc), error.error)
 
     @property
     def loc(self) -> tuple[str]:
+        """
+        Get the location where the error occurred.
+
+        Returns:
+            tuple[str]: The location represented as a tuple of strings.
+        """
         return self._loc
 
     @property
     def error(self) -> Exception:
+        """
+        Get the wrapped error instance.
+
+        Returns:
+            Exception: The wrapped error instance.
+        """
         return self._error
 
     def dump_dict(self) -> dict:
+        """
+        Convert the error information to a dictionary.
+
+        Returns:
+            dict: A dictionary containing the location and string representation of the error.
+        """
         return {
             'loc': list(self.loc),
             'error': str(self.error)
@@ -31,27 +88,96 @@ class ErrorWrapper(Exception):
 
 
 class ValidationError(Exception):
+    """
+    Exception class to represent validation errors in the mongotoy library.
+
+    Args:
+        - errors (list[ErrorWrapper]): List of ErrorWrapper instances containing details of validation errors.
+
+    Properties:
+        - errors (list[ErrorWrapper]): List of ErrorWrapper instances representing validation errors.
+
+    Methods:
+        - dump_dict(): Convert the validation error information to a dictionary for easy serialization.
+
+    Example:
+        ```python
+        try:
+            # Some code that performs data validation
+            ...
+        except ValidationError as ve:
+            # Access the list of validation errors
+            validation_errors = ve.errors
+            # Dump the validation error information as a dictionary
+            validation_error_dict = ve.dump_dict()
+        ```
+    """
 
     def __init__(self, errors: list[ErrorWrapper]):
         self._errors = errors
         super().__init__(self._get_message())
 
     def _get_message(self) -> str:
+        """
+        Get the error message for the validation exception.
+
+        Returns:
+            str: The error message indicating the number of invalid fields.
+        """
         return f'Invalid data into {len(self.errors)} fields'
 
     @property
     def errors(self) -> list[ErrorWrapper]:
+        """
+        Get the list of validation errors.
+
+        Returns:
+            list[ErrorWrapper]: List of ErrorWrapper instances representing validation errors.
+        """
         return self._errors
 
     def dump_dict(self) -> dict:
+        """
+        Convert the validation error information to a dictionary.
+
+        Returns:
+            dict: A dictionary containing the details of validation errors.
+        """
         return {
             'errors': [err.dump_dict() for err in self.errors]
         }
 
 
 class DocumentValidationError(ValidationError):
+    """
+    Exception class representing validation errors specific to a document in the mongotoy library.
 
-    def __init__(self, errors: list[ValidationError], document_cls: Type['BaseDocument']):
+    Args:
+        - errors (list[ValidationError]): List of ValidationError instances with details of document validation errors.
+        - document_cls (Type['BaseDoc']): The document class associated with the validation errors.
+
+    Properties:
+        - document_cls (Type['BaseDoc']): The document class associated with the validation errors.
+
+    Methods:
+        - dump_dict(): Convert the document validation error information to a dictionary for easy serialization.
+
+    Example:
+        ```python
+        try:
+            # Some code that performs document data validation
+            ...
+        except DocumentValidationError as dve:
+            # Access the document class
+            document_class = dve.document_cls
+            # Access the list of document validation errors
+            document_validation_errors = dve.errors
+            # Dump the document validation error information as a dictionary
+            document_validation_error_dict = dve.dump_dict()
+        ```
+    """
+
+    def __init__(self, errors: list[ValidationError], document_cls: Type['BaseDoc']):
         unwrapped_errors = []
         for err in errors:
             unwrapped_errors.extend(err.errors)
@@ -61,12 +187,24 @@ class DocumentValidationError(ValidationError):
         super().__init__(errors=unwrapped_errors)
 
     def _get_message(self) -> str:
+        """
+        Get the error message for the document validation exception.
+
+        Returns:
+            str: The error message indicating the invalid data in the document and its location.
+        """
         msg = f'Invalid data at document {self._document_path}:'
         for err in self.errors:
             msg += f'\n  - {".".join(err.loc)}: {str(err)}'
         return msg
 
     def dump_dict(self) -> dict:
+        """
+        Convert the document validation error information to a dictionary.
+
+        Returns:
+            dict: A dictionary containing the details of document validation errors.
+        """
         return {
             'document_cls': self._document_path,
             **super().dump_dict()
@@ -74,20 +212,81 @@ class DocumentValidationError(ValidationError):
 
 
 class MapperError(Exception):
+    """
+    Exception class for errors related to mapping data between different structures.
+    This can be raised when there are issues with the mapping process in the application.
+
+    Example:
+        ```python
+        try:
+            # Your code that may raise MapperError
+        except MapperError as e:
+            print(f"MapperError: {e}")
+        ```
+    """
     pass
 
 
 class FieldError(Exception):
+    """
+    Exception class for errors related to fields in a document or data structure.
+    This can be raised when there are issues with field definitions or field-specific operations.
+
+    Example:
+        ```python
+        try:
+            # Your code that may raise FieldError
+        except FieldError as e:
+            print(f"FieldError: {e}")
+        ```
+    """
     pass
 
 
 class DocumentError(Exception):
+    """
+    Exception class for errors related to documents or data structures.
+    This can be raised when there are issues with document-level operations or validations.
+
+    Example:
+        ```python
+        try:
+            # Your code that may raise DocumentError
+        except DocumentError as e:
+            print(f"DocumentError: {e}")
+        ```
+    """
     pass
 
 
 class DocumentReferenceError(Exception):
+    """
+    Exception class for errors related to document references.
+    This can be raised when there are issues with referring to or managing references between documents.
+
+    Example:
+        ```python
+        try:
+            # Your code that may raise DocumentReferenceError
+        except DocumentReferenceError as e:
+            print(f"DocumentReferenceError: {e}")
+        ```
+    """
     pass
 
 
 class EngineError(Exception):
+    """
+    Exception class for errors related to the database engine or core functionality.
+    This can be raised when there are critical issues that impact the communication with the database engine.
+
+    Example:
+        ```python
+        try:
+            # Your code that may raise EngineError
+        except EngineError as e:
+            print(f"Database Engine Error: {e}")
+        ```
+    """
     pass
+
